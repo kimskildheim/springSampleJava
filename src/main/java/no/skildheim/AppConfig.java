ac7a49227a99e1bbe592c972042ee66d22c9fc0b
Kim Skildheim <kimskildheim@gmail.com>
2017-11-29 13:45:20 +0100

    Added properties into the code from spring properties

--- src/main/java/no/skildheim/AppConfig.java
+++ src/main/java/no/skildheim/AppConfig.java
@@ -7,10 +7,18 @@ import no.skildheim.service.CustomerServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan({"no.skildheim"})
+@PropertySource("app.properties")
 public class AppConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer getPropertySourcePlaceholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
   //   @Bean(name = "customerService")
   //  public CustomerService getCustomerService(){
